d9364447fd3f1a538496f94ff3fdbaa8e1f8ecb3
krishnatcs17 <53852374+krishnatcs17@users.noreply.github.com>
2019-08-16 21:01:12 +0530

    Add files via upload

---
+++ Lab2-august16/Lab2assign2.py
@@ -0,0 +1,11 @@
+def fib(x):
+    if x is 0 or x is 1:
+        return x
+    return fib(x-1) + fib(x-2)
+
+
+n = int(input("Enter the number of total numbers to be present in Fibonacci series(minimum 1): "))
+print("Fibonacci series is: ")
+for i in range(1, n+1):
+    print(fib(i-1), end=' ')
+
