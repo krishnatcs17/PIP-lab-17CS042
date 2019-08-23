d9ad3894b163671eca8fe3058df6a0ca63aea265
krishnatcs17 <53852374+krishnatcs17@users.noreply.github.com>
2019-08-23 10:09:59 +0530

    Lab2

---
+++ Lab_aug23/lab2b.py
@@ -0,0 +1,10 @@
+def rev(arr):
+  print(arr.reverse())
+  for i in range(len(arr)):
+    arr[i] = arr[i][::-1]
+  return arr
+  
+  sent = input('Enter a long string: ')
+  arr = list(sent.split())
+  
+  print(rev(arr))
